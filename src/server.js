e64ba8d783fb63b825c17fe32aec4dcc3da02ace
joko345 <jokoprasetyo345.ju@gmail.com>
2024-12-08 09:51:41 +0000

    new

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({    
-    port: 5000,
+    port: 9000,
     host: '0.0.0.0',  
     routes: {
         cors: {
